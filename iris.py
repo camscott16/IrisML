b1dd74d9c568673c7bbbebbd65e846e7d6336a89
Cameron Scott <casctt@clemson.edu>
2024-06-02 16:36:42 -0400

    graph, model, evaluate data

--- iris.py
+++ iris.py
@@ -26,4 +26,43 @@ print(dataset.shape)
 # peek at data
 print(dataset.head(20))
 
+# class distribution
+print(dataset.groupby('class').size())
 
+# view box and whisker plots
+dataset.plot(kind = 'box', subplots = True, layout = (2, 2), sharex = False, sharey = False)
+plt.show()
+
+# view histograms
+dataset.hist()
+plt.show()
+
+# view multivarate plots
+scatter_matrix(dataset)
+plt.show()
+
+# model creation
+# split validation dataset
+array = dataset.values 
+X = array[:,0:4]
+Y = array[:,4]
+X_train, X_validation, Y_train, Y_validation, = train_test_split(X, Y, test_size = 0.20, random_state = 1)
+
+# Spot Check Algorithms
+models = []
+models.append(('LR', LogisticRegression(solver = 'liblinear', multi_class = 'ovr')))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma = 'auto')))
+
+# evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+	kfold = StratifiedKFold(n_splits = 10, random_state = 1, shuffle = True)
+	cv_results = cross_val_score(model, X_train, Y_train, cv = kfold, scoring = 'accuracy')
+	results.append(cv_results)
+	names.append(name)
+	print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
